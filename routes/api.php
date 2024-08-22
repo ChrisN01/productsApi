9331065fc63b514cd6adde58f3713ae4346ed1e8
Crisleivys Gil <crisleivysngil@gmail.com>
2024-08-22 12:37:26 -0400

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('products', ProductController::class);
+Route::prefix('products')->group(function () {
+    Route::apiResource('/{product}/reviews', ReviewController::class);
 });
